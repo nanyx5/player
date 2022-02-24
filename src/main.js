b4950cf5f829a5c8e1b7d2283b0da965e9d80b2d
南友新 <nanyx5@chinaunicom.cn>
2022-02-24 17:58:50 +0800

    音乐播放器

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(VueRouter)
+Vue.use(ElementUI);
+
+let router = new VueRouter()
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
